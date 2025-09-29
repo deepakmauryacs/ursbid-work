6c023d77f5292b1879b421fe91cd26c4af01aeb9
Dev <dm.raprocuretesting@gmail.com>
2025-09-29 11:54:41 +0530

    Fix Issue

--- resources/views/ursdashboard/active-enquiry/list.blade.php
+++ resources/views/ursdashboard/active-enquiry/list.blade.php
@@ -95,7 +95,7 @@
       <div class="row">
          <div class="col-lg-12 mb-30">
             <div class="card mb-4 shadow-sm">
-               <div class="card-header border-0 bg-white d-flex justify-content-between align-items-center flex-wrap gap-2">
+               <div class="card-header d-flex justify-content-between align-items-center border-bottom">
                   <h5 class="mb-0">Filter Enquiries</h5>
                   <button class="btn btn-outline-secondary btn-sm d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#activeEnquiryFilters" aria-expanded="true" aria-controls="activeEnquiryFilters">
                      Toggle Filters
